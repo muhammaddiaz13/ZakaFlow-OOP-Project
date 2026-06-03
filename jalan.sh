c20b3947254c0f4616785a3a8353776e964e4dac
willLL <willyambevan@gmail.com>
2026-06-03 14:50:25 +0700

    Update jalan.sh

--- jalan.sh
+++ jalan.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
 
-# Pastikan TIDAK ADA SPASI di sekitar tanda sama dengan (=)
-export DB_URL="jdbc:postgresql://db.szvoiuccuwcbguchvfxm.supabase.co:5432/postgres?user=postgres&password=Willyam13042006//"
-export DB_USERNAME="postgres"
-export DB_PASSWORD="Willyam13042006//"
-
-# Jalankan Spring Boot
-./mvnw spring-boot:run
